2669080b2161593d1cd52af3596407517231c674
Mwombeni <Adalbert.Mwombeni@ksb.com>
2020-10-23 12:08:22 +0200

    added java code that save picture in background

--- KSBSmartDevice/app/src/main/java/com/example/ksbsmartdevice/MainActivity.java
+++ KSBSmartDevice/app/src/main/java/com/example/ksbsmartdevice/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ksbsmartdevice;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.core.content.FileProvider;
@@ -36,18 +39,38 @@ public class MainActivity extends AppCompatActivity {
     Button cameraBtn, galleryBtn;
     String currentPhotoPath;
 
-    private File createdImageFile() throws IOException{
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            selectedImage.setImageBitmap(image);
+            if (resultCode == Activity.RESULT_OK) {
+                //File f = new File(currentPhotoPath);
+                //selectedImage.setImageURI(Uri.fromFile(f));
+                try {
+                    createImageFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
+    }
+
+    private void createImageFile() throws IOException {
         //create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "png_"+ timeStamp + "_";
+        String imageFileName = "png_" + timeStamp + "_";
         //File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(imageFileName, ".png", storageDir);
 
         //save a file: path for use with ACTION_VIEW intents
         currentPhotoPath = image.getAbsolutePath();
-        return image;
     }
+
 
 
     @Override
@@ -62,43 +85,33 @@ public class MainActivity extends AppCompatActivity {
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dispatchTakepicture();
-                //Test
                 Toast.makeText(MainActivity.this, "Camera is clicked", Toast.LENGTH_SHORT).show();
-                Intent camera = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                //startActivityForResult(camera, CAMERA_REQUEST_CODE);
                 openCamera();
-                //Test
+
             }
         });
 
 
-
     }
-    private  void openCamera(){
+
+    private void openCamera() {
         Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(camera, CAMERA_REQUEST_CODE);
-    }
-
-    private void dispatchTakepicture(){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         //Ensure that there is a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
-            ////create the file where the photo should go
-            File photoFile = null;
-            try{
-                createdImageFile();
-            }catch (IOException ex){
+        //if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            //create the file where the photo should go
+            //File photoFile = null;
+            //if (photoFile != null) {
+                //Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile);
+                //startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            //}
 
-            }
-            //continue only if the File was successful created
-            if (photoFile != null){
-                Uri photoURI = FileProvider.getUriForFile(this, "KSBSmartDevice", photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
-            }
-        }
+        //}
+
     }
+
 }
 
 
@@ -116,127 +129,17 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- //The second one
 /*
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == CAMERA_PERM_CODE){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                dispatchTakePictureIntent();
-            }else {
-                Toast.makeText(this, "Camera Permission is Required to Use camera.", Toast.LENGTH_SHORT).show();
-            }
-        }
+    private File createdImageFile() throws IOException {
+        //create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "png_" + timeStamp + "_";
+        //File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".png", storageDir);
+
+        //save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 */
-//The first one
-/*
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                File f = new File(currentPhotoPath);
-                selectedImage.setImageURI(Uri.fromFile(f));
-                Log.d("tag", "ABsolute Url of Image is " + Uri.fromFile(f));
-
-                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                Uri contentUri = Uri.fromFile(f);
-                mediaScanIntent.setData(contentUri);
-                this.sendBroadcast(mediaScanIntent);
-            }
-
-        }
-
-        if (requestCode == GALLERY_REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                Uri contentUri = data.getData();
-                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-                String imageFileName = "JPEG_" + timeStamp + "." + getFileExt(contentUri);
-                Log.d("tag", "onActivityResult: Gallery Image Uri:  " + imageFileName);
-                selectedImage.setImageURI(contentUri);
-            }
-
-        }
-
-
-    }
-*/
-//The third one
-/*
-    private String getFileExt(Uri contentUri) {
-        ContentResolver c = getContentResolver();
-        MimeTypeMap mime = MimeTypeMap.getSingleton();
-        return mime.getExtensionFromMimeType(c.getType(contentUri));
-    }
-*/
-
-
-//Removed items
-
-
-

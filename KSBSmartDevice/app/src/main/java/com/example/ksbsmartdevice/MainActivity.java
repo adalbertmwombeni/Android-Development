2e3c85627c1afe34a3788f2f8045155b894b342c
Mwombeni <Adalbert.Mwombeni@ksb.com>
2020-10-23 17:05:32 +0200

    added java file where the path can not be created because of permission

--- KSBSmartDevice/app/src/main/java/com/example/ksbsmartdevice/MainActivity.java
+++ KSBSmartDevice/app/src/main/java/com/example/ksbsmartdevice/MainActivity.java
@@ -1,10 +1,54 @@
 package com.example.ksbsmartdevice;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.nfc.Tag;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.security.auth.login.LoginException;
+
+
+//import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+//import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+//import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+//import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+//import com.amazonaws.services.s3.AmazonS3Client;
+
+//package com.example.ksbsmartdevice;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.nfc.Tag;
 import android.os.Bundle;
 
 import androidx.core.content.FileProvider;
@@ -13,6 +57,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -22,6 +67,8 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import javax.security.auth.login.LoginException;
 
 
 //import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
@@ -43,13 +90,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAMERA_REQUEST_CODE) {
-            Bitmap image = (Bitmap) data.getExtras().get("data");
-            selectedImage.setImageBitmap(image);
+            File photoFile = null;
             if (resultCode == Activity.RESULT_OK) {
-                //File f = new File(currentPhotoPath);
-                //selectedImage.setImageURI(Uri.fromFile(f));
+
                 try {
+                    //Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
                     createImageFile();
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -69,6 +116,9 @@ public class MainActivity extends AppCompatActivity {
 
         //save a file: path for use with ACTION_VIEW intents
         currentPhotoPath = image.getAbsolutePath();
+        File f = new File(currentPhotoPath);
+        selectedImage.setImageURI(Uri.fromFile(f));
+        Log.d("tag", "ABsolute Url of Image is " + Uri.fromFile(f));
     }
 
 
@@ -97,18 +147,6 @@ public class MainActivity extends AppCompatActivity {
     private void openCamera() {
         Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(camera, CAMERA_REQUEST_CODE);
-        //Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //Ensure that there is a camera activity to handle the intent
-        //if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            //create the file where the photo should go
-            //File photoFile = null;
-            //if (photoFile != null) {
-                //Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
-                //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile);
-                //startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
-            //}
-
-        //}
 
     }
 
@@ -128,18 +166,3 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-/*
-    private File createdImageFile() throws IOException {
-        //create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "png_" + timeStamp + "_";
-        //File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(imageFileName, ".png", storageDir);
-
-        //save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-*/

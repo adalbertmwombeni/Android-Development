60399f1874fa18e3859cb870498b0d313ae1bee8
Mwombeni <Adalbert.Mwombeni@ksb.com>
2020-11-19 14:23:13 +0100

    added final java file for android

--- KSBSmartDevice/app/src/main/java/com/example/ksbsmartdevice/MainActivity.java
+++ KSBSmartDevice/app/src/main/java/com/example/ksbsmartdevice/MainActivity.java
@@ -1,91 +1,29 @@
 package com.example.ksbsmartdevice;
-
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
 import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.nfc.Tag;
 import android.os.Bundle;
-
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
-import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import javax.security.auth.login.LoginException;
-
-
-//import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
-//import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
-//import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
-//import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
-//import com.amazonaws.services.s3.AmazonS3Client;
-
-//package com.example.ksbsmartdevice;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
-import android.graphics.Bitmap;
-import android.nfc.Tag;
-import android.os.Bundle;
-
-import androidx.core.content.FileProvider;
-
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
-
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
 import com.amazonaws.services.s3.AmazonS3Client;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javax.security.auth.login.LoginException;
-
-
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.util.IOUtils;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -95,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
     ImageView selectedImage;
     Button cameraBtn, galleryBtn;
     String currentPhotoPath;
-    private final String KEY = "A";
-    private final String SECRET = "B";
+    private final String KEY = "Some key";
+    private final String SECRET = "Some secret";
     private BasicAWSCredentials credentials;
     private AmazonS3Client s3Client;
     private Uri fileUri;
@@ -112,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
                 File f = new File(currentPhotoPath);
                 selectedImage.setImageURI(Uri.fromFile(f));
                 Log.d("tag", "ABsolute Url of Image is " + Uri.fromFile(f));
-                Toast.makeText(MainActivity.this, "====The path is also available==", Toast.LENGTH_SHORT).show();
 
                 Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
                 Uri contentUri = Uri.fromFile(f);
@@ -120,57 +57,33 @@ public class MainActivity extends AppCompatActivity {
                 fileName = f.getName();
                 mediaScanIntent.setData(contentUri);
                 this.sendBroadcast(mediaScanIntent);
-                Toast.makeText(MainActivity.this, "====>>>> The content is sent", Toast.LENGTH_SHORT).show();
                 credentials = new BasicAWSCredentials(KEY, SECRET);
                 s3Client = new AmazonS3Client(credentials);
                 uploadToAWS();
-                Toast.makeText(MainActivity.this, "==DONE==", Toast.LENGTH_SHORT).show();
-
             }
 
         }
-        //if (requestCode == CAMERA_REQUEST_CODE) {
-        //if (resultCode == Activity.RESULT_OK){
-        //File f = new File(currentPhotoPath);
-        //selectedImage.setImageURI(Uri.fromFile(f));
-
-        //Log.d("tag", "Absolute Url of image is " +Uri.fromFile(f));
-        //}
-
-        //}
     }
     // final Uri contentUri
     private void uploadToAWS() {
 
-        ////
-        Toast.makeText(MainActivity.this, "====NIRAKANANINYIRAKANANI==", Toast.LENGTH_SHORT).show();
         if (fileUri != null) {
 
-            //final File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-                   // "/");
-
-
-            //createFile(getApplicationContext(), fileUri, new File(fileName));
-
-            ////
             Log.d("tag", "the url is  " + fileUri);
-            Log.d("tag", "the file name is  " + currentPhotoPath);
             Log.d("tag", "the INPUT file is  " + new File(currentPhotoPath));
             TransferUtility transferUtility =
                     TransferUtility.builder()
                             .context(getApplicationContext())
-                            .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())   //"name", new File("Uri")
+                            .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
                             .s3Client(s3Client)
                             .build();
-
             TransferObserver uploadobserver =
                     transferUtility.upload("orsstorage1","fileUri", new File(currentPhotoPath));
-
             uploadobserver.setTransferListener(new TransferListener() {
                 @Override
                 public void onStateChanged(int id, TransferState state) {
                     if (TransferState.COMPLETED == state) {
-                        Toast.makeText(MainActivity.this, "Upload completed", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "UPLOAD COMPLETED", Toast.LENGTH_SHORT).show();
                     } else if (TransferState.FAILED == state) {
                         Toast.makeText(MainActivity.this, "FAILED", Toast.LENGTH_SHORT).show();
                     }
@@ -190,28 +103,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private String getFileExtension(Uri fileUri) {
-        ContentResolver contentResolver = getContentResolver();
-        MimeTypeMap mime = MimeTypeMap.getSingleton();
-
-        return mime.getExtensionFromMimeType(contentResolver.getType(fileUri));
-    }
-
-    private void createFile(Context applicationContext, Uri fileUri, File file) {
-        try {
-            Context context = null;
-            Uri srcUri = null;
-            InputStream inputStream = context.getContentResolver().openInputStream(srcUri);
-            if (inputStream == null) return;
-            String dstFile = null;
-            OutputStream outputStream = new FileOutputStream(dstFile);
-            IOUtils.copy(inputStream, outputStream);
-            inputStream.close();
-            outputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -227,28 +118,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Save a file: path for use with ACTION_VIEW intents
         currentPhotoPath = image.getAbsolutePath();
-        System.out.println("image");
         return image;
     }
 
-    /*
-    private void createImageFile() throws IOException {
-        //create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "png_" + timeStamp + "_";
-        //File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(imageFileName, ".png", storageDir);
-
-        //save a file: path for use with ACTION_VIEW intents
-        ////currentPhotoPath = image.getAbsolutePath();
-        ////File f = new File(currentPhotoPath);
-        ////selectedImage.setImageURI(Uri.fromFile(f));
-        ///Log.d("tag", "ABsolute Url of Image is " + Uri.fromFile(f));
-
-
-    }
-*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -262,8 +134,6 @@ public class MainActivity extends AppCompatActivity {
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Camera is clicked", Toast.LENGTH_SHORT).show();
-                //openCamera();
                 dispatchTakePictureIntent();
 
             }
@@ -280,16 +150,11 @@ public class MainActivity extends AppCompatActivity {
             File photoFile = null;
             try {
                 photoFile = createImageFile();
-                Toast.makeText(MainActivity.this, "....File is created....", Toast.LENGTH_SHORT).show();
             } catch (IOException ex) {
                 // Error occurred while creating the File
-                Toast.makeText(MainActivity.this, "....Exception is thrown....", Toast.LENGTH_SHORT).show();
-
             }
-            //Toast.makeText(MainActivity.this, "....please wait....", Toast.LENGTH_SHORT).show();
             // Continue only if the File was successfully created
             if (photoFile != null) {
-                Toast.makeText(MainActivity.this, "...I am alread in....", Toast.LENGTH_SHORT).show();
                 Uri photoURI = FileProvider.getUriForFile(this,
                         "com.example.android.fileprovider",
                         photoFile);
@@ -298,21 +163,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
